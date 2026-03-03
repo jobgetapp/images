94f79cffcc8ae7cf6d4f9db709b4509154aaf779
Sawyer Hopkins <sawyer@sawyerhopkins.com>
2026-03-02 19:14:58 -0800

    add aws lambda 22 build

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -ex
 JG_PG_VERSION=13
 JG_TIMESCALE_VERSION=2.11.0
 JG_MONGO_FDW_VERSION=5_5_0
+JG_NODE_VERSION=22
 
 cd timescale
 docker build -t ghcr.io/jobgetapp/timescale:2.11.0-pg${JG_PG_VERSION}-latest \
@@ -16,4 +17,9 @@ docker build -t ghcr.io/jobgetapp/timescale-mongofdw:${JG_MONGO_FDW_VERSION}-ts$
   --build-arg JG_PG_VERSION=${JG_PG_VERSION} \
   --build-arg JG_TIMESCALE_VERSION=${JG_TIMESCALE_VERSION} \
   --build-arg JG_MONGO_FDW_VERSION=${JG_MONGO_FDW_VERSION} \
+  .
+
+cd ../aws/lambda
+docker build -t ghcr.io/jobgetapp/aws-lambda:node${JG_PG_VERSION}-latest \
+  --build-arg JG_NODE_VERSION=${JG_NODE_VERSION} \
   .
